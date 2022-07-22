d9dbd01896c6003ed83a16b55a347364c02b164c
José Fonte <jose.fonte@cloudbeds.com>
2022-07-22 16:23:19 +0100

    small update

--- permissions/app/policies/deps.py
+++ permissions/app/policies/deps.py
@@ -1,9 +1,10 @@
 from os import getenv
 
+
 from casbin.enforcer import Enforcer
 from casbin_sqlalchemy_adapter import Adapter
 
 
 host, port, user, password, dbname = getenv("DATABASE_HOST"), getenv("POSTGRES_PORT"), getenv("POSTGRES_USER"), getenv("POSTGRES_PASSWORD"), getenv("POSTGRES_DB")
 adapter = Adapter(f"postgresql://{user}:{password}@{host}:{port}/{dbname}")
-enforcer = Enforcer("policies/ac_model.conf", adapter)
+enforcer = Enforcer("policies/model.conf", adapter)
