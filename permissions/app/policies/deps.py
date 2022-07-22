c7879452272989b7ff430b881637d1c797bb141f
José Fonte <jose.fonte@cloudbeds.com>
2022-07-22 12:24:51 +0100

    wip

--- permissions/app/policies/deps.py
+++ permissions/app/policies/deps.py
@@ -2,28 +2,5 @@ from casbin.enforcer import Enforcer
 from casbin_sqlalchemy_adapter import Adapter
 
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-
-
-
-class CasbinRule(Base):
-    __tablename__ = "policy"
-
-    id = Column(Integer, primary_key=True)
-    ptype = Column(String(255))
-    v0 = Column(String(255))
-    v1 = Column(String(255))
-    v2 = Column(String(255))
-    v3 = Column(String(255))
-    v4 = Column(String(255))
-    v5 = Column(String(255))
-
-
-adapter = Adapter("postgresql://permissions:permissions@permissions-database:5432/permissions", CasbinRule)
-
-
+adapter = Adapter("postgresql://permissions:permissions@permissions-database:5432/permissions")
 enforcer = Enforcer("policies/ac_model.conf", adapter)
--- permissions/app/policies/service.py
+++ permissions/app/policies/service.py
@@ -1,20 +1,35 @@
 from typing import List
 
 
-from policies.deps import enforcer
+from policies.deps import enforcer as enforcer_v2
 
 
 class PoliciesService:
     def get_policies_by_user_id(self, user_id: int) -> List[tuple]:
-        return enforcer.get_filtered_policy(0, str(user_id))
+        # from casbin.enforcer import Enforcer
+        # from policies.my_mom import Adapter
+
+        # adapter = Adapter("postgresql://permissions:permissions@permissions-database:5432/permissions")
+        # enforcer_v2 = Enforcer("policies/ac_model.conf", adapter)
+
+       # print(enforcer_v2.get_policy())
+        print('FIRST ---->', enforcer_v2.get_filtered_policy(0, str(user_id)))
+
+      #  enforcer_v2.add_policy("2", "reports", "write")
+       # enforcer.add_policy("2", "payments", "read")
+
+        print('SECOND ---->', enforcer_v2.get_filtered_policy(0, str(user_id)))
+
+        return enforcer_v2.get_filtered_policy(0, str(user_id))
 
     def create_policy(self, user_id: int, object: str, action: str) -> dict:
-        enforcer.add_named_policy("p", str(user_id), object, action)
-        enforcer.save_policy()
+        enforcer_v2.add_named_policy("p", str(user_id), object, action)
+        enforcer_v2.save_policy()
         return dict(user_id=str(user_id), object=object, action=action)
 
     def has_policy(self, user_id: int, object: str, action: str) -> dict:
-        if enforcer.enforce(str(user_id), object, action):
-            return dict(access=True, message=f"User {user_id} has access!")
+        return dict()
+        # if enforcer.enforce(str(user_id), object, action):
+        #     return dict(access=True, message=f"User {user_id} has access!")
 
-        return dict(access=False, message=f"User {user_id} is forbidden!")
+        # return dict(access=False, message=f"User {user_id} is forbidden!")
